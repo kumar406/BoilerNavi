be526dc315729a0b3a0a9bbd06e621b1f8e4e014
rehguerg <kumar406@purdue.edu>
2019-10-26 19:06:02 -0400

    first commit doesnt work

---
+++ app/components/Form/styles.js
@@ -0,0 +1,5 @@
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+const styles = () => null;
+
+export default styles;
---
+++ app/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+export default styles;
+import Registration from './screens/Registration';
+
+EStyleSheet.build({
+  $primaryGold: '#FFD700',
+  
+});
+
+export default () => <Registration />;
+
